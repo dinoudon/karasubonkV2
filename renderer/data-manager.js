7045f66720d327bb0f6a32e187e5c95ae22f213a
dinoudon <aldinocaturrahmanto@gmail.com>
2026-02-09 11:09:46 +0700

    dinoudon: subtask-2-1 - Extract getData and setData functions to data-mana

--- renderer/data-manager.js
+++ renderer/data-manager.js
@@ -6,6 +6,12 @@ const fs = require("fs");
 // ============
 // Handles data persistence and retrieval operations
 
+const defaultData = JSON.parse(fs.readFileSync(__dirname + "/../defaultData.json", "utf8"));
+
+// Counter for number of writes that are being attempted
+// Will only attempt to load data if not currently writing
+// Inter-process communication means this is necessary
+var isWriting = 0;
 var userDataPath = null;
 
 function initialize()
@@ -14,21 +20,52 @@ function initialize()
         userDataPath = message;
     });
     ipcRenderer.send("getUserDataPath");
+
+    ipcRenderer.on("doneWriting", () => {
+        if (--isWriting < 0)
+            isWriting = 0;
+    });
 }
 
-async function getData(key)
+// Get requested data, waiting for any current writes to finish first
+async function getData(field)
 {
-    return await ipcRenderer.invoke("getData", key);
+    while (isWriting > 0)
+        await new Promise(resolve => setTimeout(resolve, 10));
+
+    if (!fs.existsSync(userDataPath + "/data.json"))
+        fs.writeFileSync(userDataPath + "/data.json", JSON.stringify(defaultData));
+
+    var data;
+    // An error should only be thrown if the other process is in the middle of writing to the file.
+    // If so, it should finish shortly and this loop will exit.
+    while (data == null)
+    {
+        try {
+            data = JSON.parse(fs.readFileSync(userDataPath + "/data.json", "utf8"));
+        } catch {}
+    }
+    data = JSON.parse(fs.readFileSync(userDataPath + "/data.json", "utf8"));
+    var field = data[field];
+    if (typeof field === "string" || field instanceof String)
+        return field.trim();
+    return field;
 }
 
-function setData(key, value)
+// Send new data to the main process to write to file
+function setData(field, value)
 {
-    ipcRenderer.send("setData", key, value);
+    isWriting++;
+    ipcRenderer.send("setData", [ field, value ]);
+
+    if (field == "portThrower" || field == "portVTubeStudio" || field == "ipThrower" || field == "ipVTubeStudio")
+        setPorts();
 }
 
-module.exports = {
-    initialize,
-    getData,
-    setData,
-    getUserDataPath: () => userDataPath
-};
+// If ports change, write them to the file read by the Browser Source file
+async function setPorts()
+{
+    fs.writeFileSync(__dirname + "/../ports.js", "const ports = [ " + await getData("portThrower") + ", " + await getData("portVTubeStudio") + " ]; const ips = [ \"" + await getData("ipThrower") + "\", \"" + await getData("ipVTubeStudio") + "\" ];");
+}
+
+module.exports = { initialize, getData, setData, getUserDataPath: () => userDataPath };
